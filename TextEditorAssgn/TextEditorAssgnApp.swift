9cb0b5664e6b4416dec9622c70bbc3cf33c217ff
Mayank Jangid <226301131@gkv.ac.in>
2025-06-10 17:08:53 +0530

    initial commit

--- TextEditorAssgn/TextEditorAssgnApp.swift
+++ TextEditorAssgn/TextEditorAssgnApp.swift
@@ -5,13 +5,13 @@
 //  Created by Mayank Jangid on 6/10/25.
 //
 
-import SwiftUI
-
-@main
-struct TextEditorAssgnApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
+//import SwiftUI
+//
+//@main
+//struct TextEditorAssgnApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
